c7fc4f9c18829a8b5fbffd368a94a07f7ab64ee5
Muhammad Arnob <arnab@blueliner.io>
2025-02-18 12:42:36 +0600

    fix: Use ESM import to get socket.io

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//import { io } from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
+import { io } from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
 const SOCKET_API_URL = "https://bsports-socket-staging.herokuapp.com/";
 const url = window.location;
 const urlParams = new URLSearchParams(url.search);
@@ -184,7 +184,7 @@ socket.on("connect", () => {
 
 socket.on("UPDATE_SOCCER_SCORE", (payload) => {
   console.log(payload);
-  
+
   // updateSoccerScore(payload);
 });
 // Subscribe to the UPDATE_SCOREBOARD event
