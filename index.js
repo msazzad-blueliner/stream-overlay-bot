45010831ef5eccd1458f107cb7e57af0b0b0543d
Muhammad Arnob <arnab@blueliner.io>
2025-02-18 12:24:20 +0600

    test: CDN fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//import { io } from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
+import { io } from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
 const SOCKET_API_URL = "https://bsports-socket-staging.herokuapp.com/";
 const url = window.location;
 const urlParams = new URLSearchParams(url.search);
